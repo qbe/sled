05f0b4e9c8f9d2ca9476ecc72c28ec833fb4d6be
Lukas Hannen <git.c@hannen.at>
2018-05-16 14:34:24 +0200

    explanatory comments

--- sled.sh
+++ sled.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 VERSIONSTRING="SLED V 1.0.1"
+#update VERSIONSTRING for release commits
+
 if [ "$1" == "--version" ]; then echo "$VERSIONSTRING" && exit; elif [ "$1" == "--" ]; then shift; fi
+#minimal POSIX compliance code
+
 find /tmp/ -maxdepth 1 -type f -name "sled:$USER:*" \! -exec fuser -s '{}' \; -delete
+#removes every sled tempfile not currently used by any process
+
 tmpfile=$(mktemp "/tmp/sled:$USER:tmp.XXXXXXXXXX") && exec 3<> "$tmpfile" || echo "failed to create tempfile"
 editfile=$(mktemp "/tmp/sled:$USER:edit.XXXXXXXXXX") && exec 4<> "$editfile" || echo "failed to create tempfile"
+#creates necessary tempfiles and opens permanent file descriptors to them to protect them
+
 if [ "$1" != "" ]; then
     cat -- "$1" >"$tmpfile"
 fi
+#the first non-option argument is read in as a file into the primary buffer
+
 while read -r proto; do
     command=`echo "$proto" | cut -f 1 -d " "`
     line=`echo "$proto" | cut -f 1 -d " " --complement`
+    #does not use a 'IFS=" " read -r command line' construct to enable indented input
+
     if [ "$command" == "show" ]; then
         cat -n "$tmpfile" | more +"$line"
     elif [ ! -z "${command##*[!0-9]*}" ]; then
@@ -52,4 +64,6 @@ while read -r proto; do
         fi
     fi
 done
+
 exec 3>&- && exec 4>&- && find /tmp/ -maxdepth 1 -type f -name "sled:$USER:*" \! -exec fuser -s '{}' \; -delete
+#closes the protective file descriptors and repeats the deletion procedure
